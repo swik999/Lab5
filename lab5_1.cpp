b33b40e7ccbb68923098ad849cf1330ec8bcf35d
lab314-10@labeng.local <lab314-10@labeng.local>
2020-12-15 17:05:33 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+	int I = 0;
+	int J = 0;
+	int A = 1;
+	while ( A !=0 ){
+		cout << "Enter an integer: ";
+		cin >> A;
+		if (A % 2 == 0){
+			I += 1;
+		}else{
+			J += 1;
+		}
+	}
+    cout << "#Even numbers = " << I-1 << "\n";
+    cout << "#Odd numbers = " << J ;
     return 0;
-}
+	}
